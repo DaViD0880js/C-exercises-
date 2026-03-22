312a5e2f630f0f876ad4eb6ae1d03e9b89284672
Davi <75089552+DaViD0880js@users.noreply.github.com>
2026-03-22 15:40:27 -0300

    Refactor output messages in q3.c

--- questoes/q3.c
+++ questoes/q3.c
@@ -4,14 +4,14 @@ int main(void) {
 	
 	int numero, sucessor, antecessor;
 	
-	printf("Digite um numero:\n");
+	printf("Digite um numero inteiro:\n");
 	scanf("%i", &numero);
 	
 	sucessor = numero + 1;
 	
 	antecessor = numero - 1;
 	
-	printf("O sucessor de %i eh %i\nO antecessor eh %i ", numero, sucessor, antecessor);
+	printf("O sucessor de %i = %i\nO antecessor = %i ", numero, sucessor, antecessor);
 	
 	
 	return 0;
