5930ba05785003a50d24eb7fb01eac4557cae909
Davi <75089552+DaViD0880js@users.noreply.github.com>
2026-03-22 19:44:33 -0300

    Fix output message for temperature conversion

--- questoes/q8.c
+++ questoes/q8.c
@@ -4,12 +4,12 @@ int main (void) {
 
 	float celsius, fahreinheit;
 	
-	printf("Informe o valor da temperatura (em Celsius):\n");
+	printf("Informe o valor da temperatura em graus Celsius:\n");
 	scanf("%f", &celsius);
 	
 	fahreinheit = (celsius * 1.8) + 32;
 	
-	printf("A temperatura convertida em Fahreinheit = %f F", fahreinheit);
+	printf("A temperatura convertida em graus Fahreinheit = %f F", fahreinheit);
 	
 
   return 0;
