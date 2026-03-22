b7e26e3b708325cc8629c0314f5b05a14a457b5d
Davi <75089552+DaViD0880js@users.noreply.github.com>
2026-03-22 19:45:17 -0300

    Update q10.c

--- questoes/q10.c
+++ questoes/q10.c
@@ -2,17 +2,17 @@
 
 int main (void) {
 
-float distancia, tempo, velocidadeM;
+  float distancia, tempo, velocidadeM;
 
-printf("\nDigite a distancia percorrida pelo projetil em km:\n");
-scanf("%f", &distancia);
+  printf("\nDigite a distancia percorrida pelo projetil em km:\n");
+  scanf("%f", &distancia);
 
-printf("\nDigite o tempo percorrido pelo projetil em minutos:\n");
-scanf("%f", &tempo);
+  printf("\nDigite o tempo percorrido pelo projetil em minutos:\n");
+  scanf("%f", &tempo);
 
-velocidadeM = (distancia * 1000) / (tempo * 60);
+  velocidadeM = (distancia * 1000) / (tempo * 60);
 
-printf("\nO valor da velocidade media do projetil = %fm/s\n", velocidadeM);
+  printf("\nO valor da velocidade media do projetil = %fm/s\n", velocidadeM);
 
   return 0;
   
