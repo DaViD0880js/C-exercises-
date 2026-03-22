122d347b0f5014684ae502f2df263fe3856a48bd
Davi <75089552+DaViD0880js@users.noreply.github.com>
2026-03-22 19:40:48 -0300

    Update prompts for user input in q4.c

--- questoes/q4.c
+++ questoes/q4.c
@@ -4,7 +4,7 @@ int main (void) {
 
 float primeiroN, segundoN, soma;
 
-  printf("Digite um numero qualquer\n\n(Caso o numero seja decimal, use ''.''):\n");
+  printf("Digite um numero qualquer\n\n(Caso seja decimal, use ''.'' para dividir as casas):\n");
   scanf("%f", &primeiroN);
 
   printf("\n\nDigite outro numero qualquer:\n");
