170c6feefd664e2a0073decfc73e0bba5bf3616b
Davi <75089552+DaViD0880js@users.noreply.github.com>
2026-03-22 19:43:25 -0300

    Refactor input prompts and output message
    Updated prompts for clarity and improved output message.

--- questoes/q6.c
+++ questoes/q6.c
@@ -7,17 +7,17 @@ float numero1, numero2, numero3, soma, media;
   printf("Informe um numero qualquer:\n");
   scanf("%f", &numero1);
 
-  printf("Informe um segundo numero:\n");
+  printf("\nInforme um segundo numero:\n");
   scanf("%f", &numero2);
 
-  printf("Informe um terceiro numero:\n");
+  printf("\nInforme um terceiro numero:\n");
   scanf("%f", &numero3);
   
   soma = numero1 + numero2 + numero3;
   
   media = soma/3;
   
-  printf("\nA media eh: %f", media);
+  printf("\nA media aritmetica entre os 3 numeros = %f\n", media);
   //questao 6
 
 
