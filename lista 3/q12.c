c99d6a3e61554e06f85a71778df07e504ae76b0f
DaViD0880js <davimiranda0104@gmail.com>
2026-04-04 18:28:55 -0300

    Questao 12

--- lista 3/q12.c
+++ lista 3/q12.c
@@ -3,14 +3,14 @@
 
 int main (void) {
 	
-	int aluno = 0;
+	int aluno = 1;
 	float n1, n2, n3, media, maior = 0, menor = 99999;
 	
     setlocale(LC_ALL, "Portuguese");
     
     printf("--Média de 10 alunos--\n\n");
 	
-	while(aluno < 10) {
+	while(aluno <= 10) {
 		printf("Digite sua primeira nota: ");
 		scanf("%f", &n1);
 		
@@ -30,7 +30,7 @@ int main (void) {
 			menor = media;
 		}
 		
-		printf("-- Média do aluno %i = %.2f --\n\n", aluno + 1, media);
+		printf("-- Média do aluno %i = %.2f --\n\n", aluno, media);
 		
 		aluno++;
 	}
